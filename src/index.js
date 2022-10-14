f45b847710a01a2a0c56fba54d7fe0f455309222
Kushagra Singh <kushagra.singh@getfareye.com>
2022-10-14 12:22:13 +0530

    inital commit for class components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import App1 from "./App1";
+import "antd/dist/antd.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App1 />
   </React.StrictMode>
 );
 
